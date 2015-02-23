5a29f04af4a3a7673887fc36f7188d2854792cb0
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-02-22 16:23:45 -0800

    update

--- examples/sinus.py
+++ examples/sinus.py
@@ -1,11 +1,16 @@
 from math import factorial
 
+# hangs on mutant
 def my_sin(x):
+    """
+    >>> my_sin(1)
+    0.8416666666666667
+    """
     sign = -1
     p = d = 1
     i = sinx = 0
 
-    while d > 0.00001: 
+    while d > 0.01:
         d = (x**p)/float(factorial(p))
         sinx += ((sign**i)*d)
         i+=1
