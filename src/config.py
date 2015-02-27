3bb83afbd0735295b22871a9430eb5ee5abf16c6
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-02-27 09:22:04 -0800

    update

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@
 # optimistic and fails to handle cases where we have exception handlers that
 # are overly aggressive with 'except:' -- the sigalarm we use gets stuck in
 # these exception handlers.
-WaitSingleFn = 2
-WaitSingleMutant = 60 * 5
-WaitTestRun = WaitSingleFn * 10
+WaitSingleFn = 5
+WaitTestRun = 10 * WaitSingleFn
+WaitSingleMutant = 100 * WaitSingleFn
 MaxTries = 10000
 MaxSpace = 1000000
 FnRes = dict(TimedOut=0,Detected=1, NotEq=2,ProbEq=3)
