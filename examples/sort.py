85aef3892d9f5f5383fc47c669be7dd16c44bcd7
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-02-27 02:11:34 -0800

    update

--- examples/sort.py
+++ examples/sort.py
@@ -307,3 +307,32 @@ def binary_sort(items):
     items[initial_left + index] = value
   return items
 
+@typ.typ(items=[int])
+def count_sort(items):
+  """
+  >>> count_sort([])
+  []
+  >>> count_sort([1])
+  [1]
+  >>> count_sort([2,1])
+  [1, 2]
+  >>> count_sort([1,2])
+  [1, 2]
+  >>> count_sort([1,2,2])
+  [1, 2, 2]
+  """
+  counts = {}
+  for num in items:
+    if num in counts:
+      counts[num] += 1
+    else:
+      counts[num] = 1
+
+  sorted_list = []
+  for num in xrange(min(items), max(items) + 1):
+    if num in counts:
+      for j in xrange(counts[num]):
+        sorted_list.append(num)
+
+  return sorted_list
+
