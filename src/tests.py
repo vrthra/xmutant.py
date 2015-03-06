de05c98f891557322ac0762133e8c6ca763b48fb
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-03-06 14:02:15 -0800

    verbose

--- src/tests.py
+++ src/tests.py
@@ -13,12 +13,12 @@ def runAllTests(module, msg):
   runner = doctest.DocTestRunner(verbose=False)
   for test in finder.find(module, module.__name__):
     try:
-      out().debug("Test M[%s] ->%s  %s:%s (%s)" % (os.getpid(), test.name, test.filename, test.lineno, msg))
+      out().debug("Test All[%s] ->%s  %s:%s (pid:%s)" % (os.getpid(), test.name, test.filename, test.lineno, msg))
       with alarm.Alarm(config.t['WaitTestRun']):
         runner.run(test, out=pr)
-      out().debug("Test M[%s] <-%s failed:%s/%s (%s)" % (os.getpid(), test.name, runner.failures, runner.tries, msg))
+      out().debug("Test All[pid:%s] <-%s failed:%s/%s (%s)" % (os.getpid(), test.name, runner.failures, runner.tries, msg))
     except alarm.Alarm.Alarm:
-      out().debug("Test M[%s] #-%s %s" % (os.getpid(), test.name, msg))
+      out().debug("Test All[pid:%s] #-%s %s" % (os.getpid(), test.name, msg))
       return False # timeout!
     if runner.failures > 0: return False
   return True
